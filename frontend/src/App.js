02c35e27113588c7beaf4a8c0a24ce33f050e223
unknown <onifadejohnson2017@gmail.com>
2023-01-28 13:26:21 -0500

    Initial file

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;

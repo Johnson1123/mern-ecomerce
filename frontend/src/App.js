5f9fcb23f4010242af9881c8aa375ded11f64398
unknown <onifadejohnson2017@gmail.com>
2023-01-28 14:45:47 -0500

    Router Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { Children } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet
+} from "react-router-dom";
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 import { Counter } from './features/counter/Counter';
+import { Cart, Home, Product, NotFound } from './pages';
+
+const Layout = () => {
+  return <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/product",
+        element: <Product />
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/",
+        element: <Home />
+      },
+    ]
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,4 @@
+export { Cart } from './Cart'
+export { Product } from './Product'
+export { Home } from './Home'
+export { NotFound } from './NotFound'
